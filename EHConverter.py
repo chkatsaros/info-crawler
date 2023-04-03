ca13d1f25ab753830a5c1bc6c015e1f72aec9e3f
chkatsaros <chris.d.katsaros@gmail.com>
2023-04-03 21:49:07 +0300

    Update EHConverter

--- EHConverter.py
+++ EHConverter.py
@@ -1,5 +1,4 @@
 import json
-import csv
 import xmltodict
 
 # XML to JSON conversion
@@ -12,27 +11,3 @@ json_data = json.dumps(data_dict)
 json_file = open("example.json", "w") 
 json_file.write(json_data)
 json_file.close()
-
-# JSON to CSV conversion
-json_file = open("example.json" )
-data = json.load(json_file)
- 
-eh_data = data['EmailHarvester']
- 
-# now we will open a file for writing
-csv_file = open('example.csv', 'w')
- 
-# create the csv writer object
-csv_writer = csv.writer(csv_file)
-
-# Writing headers of CSV file
-header = 'email'
-csv_writer.writerow([header])
-
-for email in eh_data['email']:
-    # Writing data of CSV file
-    csv_writer.writerow([email])
- 
-xml_file.close()
-json_file.close()
-csv_file.close()
